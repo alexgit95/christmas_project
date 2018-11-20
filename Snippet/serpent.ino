24f13414417b3d70d189f1b0b8f6465bc71f8d57
alexgit95 <45203119+alexgit95@users.noreply.github.com>
2018-11-20 14:48:54 +0100

    FEATURE - Ajout partie SNIPPET et serpent

---
+++ Snippet/serpent.ino
@@ -0,0 +1,44 @@
+ //SERPENT - AB 2018
+ 
+const int NOMBRE_PIXEL=133;//nombre de pixel sur la bande
+const int TAILLE_SERPENT=5;//longueur en pixel du serpent
+const int DELAIS_ENTRE_MOUVEMENT=10;//(en ms) plus le delais est long, plus le serpent va lentement
+const int NB_ALLER_RETOUR=3;//nombre d'aller retour avant de commencer une nouvelle boucle
+
+
+ 
+void setup() {
+  delay(2000);//sanity check
+
+}
+
+void loop() {
+    
+    int nbar=0;//nombre d'aller retour en cours
+    int positiontete=TAILLE_SERPENT+1;
+    boolean sensVersGauche=true;
+    
+    while(nbar<NB_ALLER_RETOUR) {
+      
+        if(positiontete==NOMBRE_PIXEL-TAILLE_SERPENT||positiontete==TAILLE_SERPENT) {
+          if(!sensVersGauche) {
+            nbar++;
+          }
+          sensVersGauche=!sensVersGauche;
+        }
+        if(sensVersGauche) {
+          positiontete++;
+         //On eteint position tete - taille serpent
+          
+        }else {
+          positiontete--;
+         //On eteint position tete + taille serpent
+        }
+        //on allume position tete
+        
+        delay(DELAIS_ENTRE_MOUVEMENT);
+        
+      
+    }
+
+}
